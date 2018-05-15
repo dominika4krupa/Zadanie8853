35e159ce1299ca75da8937623723fe60ae79c50c
Dominika Krupa <doniaskrzypek@gmail.com>
2018-05-15 21:32:45 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    changeBackground();
+});
+
+$(window).scroll(function() {
+    changeBackground();
+})
+
+function changeBackground() {
+    let navHeight = $('#main-nav').outerHeight();
+    let currentPosition = $(window).scrollTop();
+
+    if(currentPosition >= navHeight) {
+        $('#main-nav').addClass('scrolled');
+    } else {
+        $('#main-nav').removeClass('scrolled');
+    }
+}
